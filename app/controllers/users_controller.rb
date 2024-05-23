bdff33fa132e821b34a1bd276a2c7e211f847525
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-05-23 16:53:11 +0000

    user_showユーザー画像まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def edit
   end
 
   def confirm
+  end
+  
+  private
+  
+  def user_params
+    params.repuire(:user).permit(:name, :profile_image)
   end
 end
