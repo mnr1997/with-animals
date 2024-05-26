2758f66b4e693d40022cb7956843c9d2b9928647
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-05-26 04:42:04 +0000

    いいね機能

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :image, presence: true
   validates :caption, presence: true
@@ -12,5 +13,9 @@ class Post < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image/jpg', content_type: 'imege/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_05_26_025823) do
+ActiveRecord::Schema.define(version: 2024_05_26_040015) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2024_05_26_025823) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment", null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
