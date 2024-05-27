85ed2ec9b39c0d20848ae17b3395cf8fde2f9df5
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-05-27 16:38:34 +0000

    2024.5.27休憩

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,13 +1,25 @@
 class AnimalsController < ApplicationController
   def new
+    @animal = Animal.new
+  end
+  
+  def create
+    
   end
 
   def index
+    @animals = Animal.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def animal_params
+    params.require(:animal).permit(:image, :name, :sex, :age, :character)
   end
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,10 @@
 class Animal < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  
+  has_one_attached :animal_image
+  
+  def get_animal_image(width, height)
+    
+  end
 end
