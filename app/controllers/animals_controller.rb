65a2f026e74b53bb390c6e0478d927ae956cec7e
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-06-01 15:32:42 +0000

    動物編集

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -7,15 +7,16 @@ class AnimalsController < ApplicationController
     @animal = Animal.new(animal_params)
     @animal.user_id = current_user.id
     if @animal.save
-      redirect_to posts_path
+      user = User.find(params[:user_id])
+      redirect_to user_animals_path(user.id)
     else
       render :new
     end
   end
 
   def index
-    user = User.find(params[:user_id])
-    @animals = user.animals
+    @user = User.find(params[:user_id])
+    @animals = @user.animals
   end
 
   def show
@@ -24,8 +25,20 @@ class AnimalsController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:user_id])
+    @animal = Animal.find(params[:id])
   end
-
+  
+  def update
+    user = User.find(params[:user_id])
+    animal = Animal.find(params[:id])
+    if animal.update(animal_params)
+      redirect_to user_animal_path(user_id: animal.user_id, id: animal.id)
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def animal_params
