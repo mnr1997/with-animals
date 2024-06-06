544d73a1f50c1542254ee958a922bff580deee1b
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-06-06 16:47:24 +0000

    admin機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
+  
   root to: "homes#top"
   devise_for :users
   devise_scope :user do
