9c1b26e5faf4dd12ab5a7f3dc98a9eefa3bc026f
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-06-13 00:55:18 +0000

    postレイアウト1

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,31 +1,25 @@
 class Public::RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
     redirect_to user_path(user)
   end
-  
+
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow(user)
     redirect_to user_path(user)
   end
-  
+
   def following
     @user = User.find(params[:user_id])
     @users = @user.following_users
   end
-  
+
   def followed
     @user = User.find(params[:user_id])
     @users = @user.followed_users
-  end
-  
-  private
-  
-  def relationship_params
-    params.require(:relationship).permit(:following_user_id, :followed_user_id)
   end
 end
