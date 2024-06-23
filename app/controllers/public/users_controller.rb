a537f689f966bcabcc61158a0df8470ad102e023
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-06-23 15:05:23 +0000

    レイアウト調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
-  before_action :ensure_guest_user, only: [:edit]
+  
   
   def index
     if params[:name].present?
@@ -42,10 +42,5 @@ class Public::UsersController < ApplicationController
     end
   end
   
-   def ensure_guest_user
-     @user = User.find(params[:id])
-     if @user.guest_user?
-       redirect_to user_path(current_user)
-     end
-    end
+   
 end
