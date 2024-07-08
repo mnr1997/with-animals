3023561d70cbb0cac9c3d3d4c86663234128be6f
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-07-08 00:16:07 +0000

    拡張子指定

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,9 +3,9 @@ class Animal < ApplicationRecord
   belongs_to :category, optional: true
   has_many :animal_posts, dependent: :destroy
   has_many :posts, through: :animal_posts, source: :post
-  
+
   enum sex: { male: 0, female: 1, unknown: 2 }
-  
+
   validates :name, presence: true
   validates :name, length: { maximum: 20 }
 
@@ -14,7 +14,7 @@ class Animal < ApplicationRecord
   def get_animal_image(width, height)
     unless animal_image.attached?
       file_path = Rails.root.join("app/assets/images/paw.jpg")
-      animal_image.attach(io: File.open(file_path), filename: "default-image/jpg", content_type: "imege/jpg")
+      animal_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "imege/jpg")
     end
     animal_image.variant(resize_to_limit: [width, height]).processed
   end
