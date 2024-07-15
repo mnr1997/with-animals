c95127fded33953628088c70065baeb72ef3e989
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-07-15 15:32:26 +0000

    rank修正

--- app/controllers/public/ranks_controller.rb
+++ app/controllers/public/ranks_controller.rb
@@ -1,6 +1,5 @@
 class Public::RanksController < ApplicationController
   def rank
-    rank_post_ids = Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id)
-    @rank = Post.where(id: rank_post_ids).page(params[:page])
+    @posts = Post.joins(:favorites).group('posts.id').order('COUNT(favorites.id) DESC').page(params[:page])
   end
 end
