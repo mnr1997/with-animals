3c429dae4caf27a26ddf97278b4731b1e67e64a8
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-08-31 15:06:32 +0000

    following_user_controller

--- app/controllers/public/following_users_controller.rb
+++ app/controllers/public/following_users_controller.rb
@@ -1,6 +1,6 @@
-# class Public::FollowingUsersController < PublicController
+class Public::FollowingUsersController < PublicController
 #   def index
 #     @user = User.find(params[:user_id])
 #     @users = @user.following_users.page(params[:page])
 #   end
-# end
+end
