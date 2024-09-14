4ce2f26f88c8e25cdd467a5d99be50e25ebb0f34
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-09-14 15:34:56 +0000

    バリデーション修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,7 @@ class Category < ApplicationRecord
   has_many :animals
   has_many :category_posts, through: :animals, source: :posts
 
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  validates :name, length: { maximum: 20 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 20 }
   
   scope :name_search, -> (name) do
     where("name LIKE ?", "%#{ActiveRecord::Base.sanitize_sql_like(name)}%")
