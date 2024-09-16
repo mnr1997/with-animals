1aa19335e810a7c1e3b9849ad2e45044bec4de85
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-09-16 02:03:54 +0000

    コメント削除非同期化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -9,7 +9,7 @@ class Public::CommentsController < PublicController
 
   def destroy
     Comment.find(params[:id]).destroy!
-    redirect_to post_path(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   private
