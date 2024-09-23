d4d1b087943a34d233ca2d8f5a088c68edb97f11
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-09-23 15:37:23 +0000

    RSpec/postコントローラ一覧画面テスト途中

---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "投稿のテスト" do
+  let!(:post) { FactoryBot.build(:post) }
+  
+  describe "一覧画面のテスト" do
+    before do 
+      visit posts_path
+    end
+    
+    context "URLが正しい" do
+      it "URLが正しい" do
+        expect(current_path).to eq '/books'
+      end
+    end
+  end
+end
