de05efabbd98314cf8c41b3322c4d1177c387302
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-09-26 15:03:58 +0000

    新規登録テスト

--- spec/system/before_login_spec.rb
+++ spec/system/before_login_spec.rb
@@ -44,4 +44,53 @@ describe "ユーザーログイン前のテスト" do
       end
     end
   end
+  
+  describe "ユーザー新規登録のテスト" do
+    before do
+      visit new_user_registration_path
+    end
+    
+    context "表示内容の確認" do
+      it "URLが正しい" do
+        expect(current_path).to eq "/users/sign_up"
+      end
+      
+      it "「新規登録」と表示される" do
+        expect(page).to have_content "新規登録"
+      end
+      
+      it "名前フォームが表示される" do
+        expect(page).to have_field "user[name]"
+      end
+      it "メールアドレスフォームが表示される" do
+        expect(page).to have_field "user[email]"
+      end
+      it "パスワードフォームが表示される" do
+        expect(page).to have_field "user[password]"
+      end
+      it "パスワード確認フォームが表示される" do
+        expect(page).to have_field "user[password_confirmation]"
+      end
+      it "登録ボタンが表示される" do
+        expect(page).to have_button "登録"
+      end
+    end
+    
+    context "新規登録が成功" do
+      before do
+        fill_in "user[name]", with: Faker::Lorem.characters(number: 10)
+        fill_in "user[email]", with: Faker::Internet.email
+        fill_in "user[password]", with: "password"
+        fill_in "user[password_confirmation]", with: "password"
+      end
+      
+      it "正しく新規登録される" do
+        expect { click_button "登録" }.to change(User.all, :count).by(1)
+      end
+      it "新規登録後のリダイレクト先が投稿一覧画面になっている" do
+        click_button "登録"
+        expect(current_path).to eq "/posts"
+      end
+    end
+  end
 end
