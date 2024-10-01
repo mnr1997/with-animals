ce267f02eeb38de4901c13e4ad524a4ce97f2089
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-10-01 14:20:00 +0000

    ログイン後ヘッダーテスト

--- spec/system/after_login_spec.rb
+++ spec/system/after_login_spec.rb
@@ -2,17 +2,72 @@
 
 require "rails_helper"
 
-describe "ユーザーログイン前のテスト" do
-  let!(:post) { FactoryBot.build(:post) }
+describe "ユーザーログイン後のテスト" do
+  let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
+  let!(:post) { create(:post, user: user) }
+  let!(:other_book) { create(:post, user: other_user) }
   
-  describe "一覧画面のテスト" do
-    before do 
-      visit posts_path
+  before do
+    visit new_user_session_path
+    fill_in "user[email]", with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'ログイン'
+  end
+  
+  describe "ヘッダーのテスト" do
+
+    context "表示内容の確認" do
+      it "ヘッダーロゴが表示される: 左上から1番目のリンクが「with Animals」である" do
+        home_link = find_all("a")[0].text
+        expect(home_link).to match(/with Animals/)
+      end
+
+      it "新規登録リンクが表示される: 左上から2番目のリンクが「新規投稿」である" do
+        home_link = find_all("a")[1].text
+        expect(home_link).to match(/新規投稿/)
+      end
+
+      it "ログインリンクが表示される: 左上から3番目のリンクが「ログアウト」である" do
+        home_link = find_all("a")[2].text
+        expect(home_link).to match(/ログアウト/)
+      end
+      
+      it "ログインリンクが表示される: 左上から4番目のリンクがユーザープロフィール、ユーザー名である" do
+        home_link = find_all("a")[3].text
+        expect(home_link).to match(user.name)
+      end
     end
-    
-    context "URLが正しい" do
-      it "URLが正しい" do
-        expect(current_path).to eq "/books"
+
+    context "リンク内容の確認" do
+      subject { current_path }
+
+      it "ヘッダーロゴを押すと、投稿一覧画面に遷移する" do
+        home_link = find_all("a")[0].text
+        home_link.gsub!(/\n/, "")
+        click_link home_link
+        is_expected.to eq "/posts"
+      end
+      
+      it "新規投稿を押すと、新規投稿画面に遷移する" do
+        home_link = find_all("a")[1].text
+        home_link.gsub!(/\n/, "")
+        click_link "新規投稿"
+        is_expected.to eq "/posts/new"
+      end
+      
+      it "ログアウトを押すと、ホーム画面に遷移する" do
+        home_link = find_all("a")[2].text
+        home_link.gsub!(/\n/, "")
+        click_link "ログアウト"
+        is_expected.to eq "/"
+      end
+      
+      it "プロフィール画像、ユーザー名を押すと、ユーザー詳細画面に遷移する" do
+        home_link = find_all("a")[2].text
+        home_link.gsub!(/\n/, "")
+        click_link user.name, match: :first
+        is_expected.to eq "/users/" + user.id.to_s
       end
     end
   end
